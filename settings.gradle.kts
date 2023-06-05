9e8312e0ecf84775f4d17801c11ca59c575dd715
GitHub Action <action@github.com>
2023-06-05 06:17:33 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "where_ref"
--- src/main/kotlin/com/github/czl92/whereref/services/MyProjectService.kt
+++ src/main/kotlin/com/github/czl92/whereref/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.czl92.whereref.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.czl92.whereref.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
